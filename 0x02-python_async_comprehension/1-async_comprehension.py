e38995a134e1483964f896dd930844af53203497
Ogundare Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-03 13:44:04 +0100

    Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,8 @@ import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> float:
-    """ Async Comprehensions
+    """ collect 10 random numbers using an async 
+        comprehensing over async_generator
     """
-    return [i async for i in async_generator()]
+    result = [i async for i in async_generator()]
+    return result
