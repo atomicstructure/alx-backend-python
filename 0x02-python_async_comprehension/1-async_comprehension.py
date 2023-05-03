1e4f75b41ef35c8315a52a83341200966d355223
Ogundare Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-03 13:38:37 +0100

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-""" Async Comprehensions """
+""" Async Comprehensions 
+    Write a coroutine called async_comprehension that takes no arguments.
+"""
 import asyncio
-import random
 
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> float:
-    """ Async Comprehensions """
+    """ Async Comprehensions
+    """
     return [i async for i in async_generator()]
